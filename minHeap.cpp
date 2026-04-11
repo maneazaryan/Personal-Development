cb69118517c2996a05e46801f103e381ab2ad7c3
Mane Azaryan <azaryanmane@gmail.com>
2026-04-11 16:17:28 +0400

    implemant symbol pair validation

---
+++ minHeap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+class MinHeap
+{
+    public:
+        MinHeap();
+        void push(int num);//add
+        void pop();// delete root
+        int top();// get root?
+        int size(){return size;};
+
+    private:
+        
+};
+
+int main()
+{
+    return 0;
+}
